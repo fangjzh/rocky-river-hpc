9ee9a6995e43da5878e912dd4529eb557ef3db2d
sam <fangjzh@foxmail.com>
2021-07-08 16:39:05 +0800

    first commit

---
+++ openhpc_script/env_tmp.sh
@@ -0,0 +1,18 @@
+export sms_name=cjhpc
+export sms_ip=10.0.0.1
+export sms_eth_internal=enp0s8
+export eth_provision=enp0s8
+export internal_netmask=255.255.255.0
+export internal_netmask_l=24
+export ntp_server=10.0.0.1
+
+export c_ip_pre=10.0.0.1
+
+export sms_ipoib=10.0.1.1
+export ipoib_netmask=255.255.255.0
+export c_ipoib_pre=10.0.1.1
+
+export compute_prefix=cnode
+export kargs=net.ifnames=1
+
+export CHROOT=/opt/ohpc/admin/images/rocky8.4
