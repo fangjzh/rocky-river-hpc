951175b78538467ff2bb0ac1eb47e5e40eecc017
sam <fangjzh@foxmail.com>
2021-07-09 13:10:36 +0800

    fix something 2

--- openhpc_script/openhpc-rocky-warewulf-0.1.sh
+++ openhpc_script/openhpc-rocky-warewulf-0.1.sh
@@ -309,20 +309,31 @@ yum -y install lmod-defaults-gnu9-openmpi4-ohpc
 cd ${package_dir}
 sh l_BaseKit_p_2021.3.0.3219_offline.sh -x 
 cd l_BaseKit_p_2021.3.0.3219_offline
-./install.sh --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
+##./install.sh --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
+./install.sh --components intel.oneapi.lin.dpcpp-cpp-compiler:intel.oneapi.lin.mkl.devel  --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
 
 cd ${package_dir}
 sh l_HPCKit_p_2021.3.0.3230_offline.sh -x
 cd l_HPCKit_p_2021.3.0.3230_offline
-./install.sh --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
+##./install.sh --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
+./install.sh --components intel.oneapi.lin.ifort-compiler:intel.oneapi.lin.dpcpp-cpp-compiler-pro:intel.oneapi.lin.mpi.devel --install-dir=/opt/ohpc/pub/apps/intel --silent --eula accept
+
+#./install.sh --list-products
+#./install.sh --list-components --product-id intel.oneapi.lin.basekit.product --product-ver 2021.3.0-3219
+#./install.sh --list-components --product-id intel.oneapi.lin.hpckit.product --product-ver 2021.3.0-3230
 
 #the pacakge is installed as big as 22G, uninstaller is in /opt/intel##
+# install tmpfiles is in /opt/intel/oneapi/installer, can repair install #
+
 ## how to make module###
 /opt/ohpc/pub/apps/intel/modulefiles-setup.sh
 
-##export MODULEPATH=${MODULEPATH}:/opt/ohpc/pub/apps/intel/modulefiles
+echo 'export MODULEPATH=${MODULEPATH}:/opt/ohpc/pub/apps/intel/modulefiles' >> /etc/profile.d/lmod.sh
+
+## this command is not 
+pdsh -w ${compute_prefix}0[1-2]  echo 'export MODULEPATH=\${MODULEPATH}:/opt/ohpc/pub/apps/intel/modulefiles' \>\> /etc/profile.d/lmod.sh
 ## also can add in /etc/profile.d/lmod.sh
-export MODULEPATH=${MODULEPATH}:/opt/ohpc/pub/apps/intel/compiler/latest/modulefiles:/opt/ohpc/pub/apps/intel/mkl/latest/modulefiles:/opt/ohpc/pub/apps/intel/mpi/latest/modulefiles:/opt/ohpc/pub/apps/intel/tbb/latest/modulefiles
+##export MODULEPATH=${MODULEPATH}:/opt/ohpc/pub/apps/intel/compiler/latest/modulefiles:/opt/ohpc/pub/apps/intel/mkl/latest/modulefiles:/opt/ohpc/pub/apps/intel/mpi/latest/modulefiles:/opt/ohpc/pub/apps/intel/tbb/latest/modulefiles
 
 
 
