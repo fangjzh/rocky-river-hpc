76193ab7f044ee2957bd4e37f0b577af3a39ceaa
sam <fangjzh@foxmail.com>
2021-07-12 07:09:37 +0800

    improve script

--- openhpc_script/openhpc-rocky-xcat-statefull-0.1.sh
+++ openhpc_script/openhpc-rocky-xcat-statefull-0.1.sh
@@ -329,42 +329,44 @@ nodeset compute osimage=${image_choose}
 ##############################################################################
 #########################################################################
 ##############################################################################
-## now add components to cnode02-node
+##############################################################################
+## now add components to ${nodename}-node
 export sms_ip=10.0.0.3
 export pacakge_dir=/root/
+export nodename=cnode02
 
-psh cnode02 perl -pi -e '"s/enabled=1/enabled=0/"' /etc/yum.repos.d/\*.repo
-psh cnode02 wget -O /etc/yum.repos.d/compute_node.repo http://${sms_ip}:80//opt/repo/compute_node.repo
-psh cnode02 yum clean all
+psh ${nodename} perl -pi -e '"s/enabled=1/enabled=0/"' /etc/yum.repos.d/\*.repo
+psh ${nodename} wget -O /etc/yum.repos.d/compute_node.repo http://${sms_ip}:80//opt/repo/compute_node.repo
+psh ${nodename} yum clean all
 
 
-###install software into cnode02 node ###
-psh cnode02 yum -y install ohpc-base-compute.x86_64
-# Disable firewall for cnode02s
-psh cnode02 systemctl disable firewalld
+###install software into ${nodename} node ###
+psh ${nodename} yum -y install ohpc-base-compute.x86_64
+# Disable firewall for ${nodename}s
+psh ${nodename} systemctl disable firewalld
 
 #####install module environment #####
-psh cnode02 yum -y install lmod-ohpc
+psh ${nodename} yum -y install lmod-ohpc
 
 # Add Slurm client support meta-package
-psh cnode02 yum -y install munge ohpc-slurm-client
-psh cnode02 systemctl  enable munge 
-psh cnode02 systemctl  enable slurmd
-psh cnode02 echo SLURMD_OPTIONS="--conf-server ${sms_ip}" \> /etc/sysconfig/slurmd
+psh ${nodename} yum -y install munge ohpc-slurm-client
+psh ${nodename} systemctl  enable munge 
+psh ${nodename} systemctl  enable slurmd
+psh ${nodename} echo SLURMD_OPTIONS="--conf-server ${sms_ip}" \> /etc/sysconfig/slurmd
 
 
 # Add Network Time Protocol (NTP) support
-psh cnode02 yum -y  install chrony
-psh cnode02 systemctl enable chronyd
+psh ${nodename} yum -y  install chrony
+psh ${nodename} systemctl enable chronyd
 # Identify master host as local NTP server
 ##cd ${package_dir}
 ##echo "server ${sms_ip}" > ./chrony.conf
-##pscp ./chrony.conf cnode02:/etc/
-psh cnode02  echo "server ${sms_ip}" \>\> /etc/chrony.conf
+##pscp ./chrony.conf ${nodename}:/etc/
+psh ${nodename}  echo "server ${sms_ip}" \>\> /etc/chrony.conf
 
 ##################### add autofs #################################
-psh cnode02 yum -y install autofs
-psh cnode02 systemctl enable autofs
+psh ${nodename} yum -y install autofs
+psh ${nodename} systemctl enable autofs
 
 ##autofs ##
 # pacakge_dir=/root/package
@@ -379,27 +381,27 @@ echo "/opt/repo        ${sms_ip}:/opt/repo" > ./auto.repo
 echo "*    ${sms_ip}:/home/&" > ./auto.home
 
 
-## scp file , cnode02 is node group ##
-pscp ./auto.* cnode02:/etc/
+## scp file , ${nodename} is node group ##
+pscp ./auto.* ${nodename}:/etc/
 ##################### add autofs  end #################################
 
 
-# Update memlock settings within cnode02 image, this is not wort with psh 
-psh cnode02 perl -pi -e "'s/# End of file/\* soft memlock unlimited\n$&/s'" /etc/security/limits.conf
-psh cnode02 perl -pi -e "'s/# End of file/\* hard memlock unlimited\n$&/s'" /etc/security/limits.conf
+# Update memlock settings within ${nodename} image, this is not wort with psh 
+psh ${nodename} perl -pi -e "'s/# End of file/\* soft memlock unlimited\n$&/s'" /etc/security/limits.conf
+psh ${nodename} perl -pi -e "'s/# End of file/\* hard memlock unlimited\n$&/s'" /etc/security/limits.conf
 #####
 
 # Enable ssh control via resource manager
-psh cnode02  echo "account required pam_slurm.so" \>\> /etc/pam.d/sshd
+psh ${nodename}  echo "account required pam_slurm.so" \>\> /etc/pam.d/sshd
 
-###### reboot the cnode02 node ####
-######        the cnode02 node ####
-######            cnode02 node ####
+###### reboot the ${nodename} node ####
+######        the ${nodename} node ####
+######            ${nodename} node ####
 ######                    node ####
 ######                         ####
 
-## xdcp cnode02 /etc/slurm/slurm.conf /etc/slurm/slurm.conf
-xdcp cnode02 /etc/munge/munge.key /etc/munge/munge.key
+## xdcp ${nodename} /etc/slurm/slurm.conf /etc/slurm/slurm.conf
+xdcp ${nodename} /etc/munge/munge.key /etc/munge/munge.key
 
 #
 # Create a sync file for pushing user credentials to the nodes
@@ -408,7 +410,7 @@ echo "/etc/passwd -> /etc/passwd" >> syncusers
 echo "/etc/group -> /etc/group" >> syncusers
 echo "/etc/shadow -> /etc/shadow" >> syncusers
 # Use xCAT to distribute credentials to nodes
-xdcp cnode02 -F syncusers
+xdcp ${nodename} -F syncusers
 
 
 ###  the 2nd way to sync users ####
@@ -425,7 +427,7 @@ xdcp cnode02 -F syncusers
 
 ## when unsers are added , execute the command 
 ## updatenode cnode02 -F
-###  updatenode compute -F
+###  updatenode compute -F  ### compute is the group name
 
 ### The “updatenode compute -F” command can be used to distribute changes made 
 ### to any defined synchro-nization files on the SMS host.  
