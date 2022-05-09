256db1f57bd701b06e02ccf9eed52b32dbfd7886
sam <fangjzh@foxmail.com>
2022-05-09 23:51:02 +0800

    fix ipmi rflash

--- openhpc_script/openhpc-rocky-xcat-statefull-0.3.2/xcat_2.16.4_crack_vbmc/xCAT_plugin/ipmi.pm
+++ openhpc_script/openhpc-rocky-xcat-statefull-0.3.2/xcat_2.16.4_crack_vbmc/xCAT_plugin/ipmi.pm
@@ -2606,7 +2606,7 @@ sub start_rflash_processes {
     }
     my $rflash_status;
     foreach (@donargs) {
-        do_rflash_process($_->[0], $_->[1], $_->[2], $_->[3], $_->[4],
+        do_rflash_process($_->[0], $_->[1], $_->[2], $_->[3], $_->[4], $_->[5],
             $ipmitimeout, $ipmitrys, $command, -args => \@exargs);
     }
 
