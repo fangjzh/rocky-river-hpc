d5d81e9e65d3d09ef31fff616be1d02b6a27fcba
jingzhong fang <fangjzh@foxmail.com>
2022-11-16 19:31:45 +0800

    初步建立脚本思路

---
+++ Rocky-River/functions/findpackage.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+res_tmp=(`find /root /mnt /media -name Rocky-8.5-x86_64-dvd1.iso`)
+if [ -z ${res_tmp[0]} ]; then
+  echo "没有找到系统镜像！"
+exit 11
+else
+  iso_path_f=`realpath  ${res_tmp[0]}`
+  iso_path=${iso_path_f%/*}
+fi
+
+res_tmp=(`find /root /mnt /media -name dep-packages.tar`)
+if [ -z ${res_tmp[0]} ]; then
+  echo "没有找到安装包文件！"
+  exit 12
+else
+  package_dir_f=`realpath  ${res_tmp[0]}`
+  package_dir=${package_dir_f%/*}
+fi
+
+#### 检查所有安装包 ###
+filelist=(
+dep-packages.tar
+kickstart-powertools.tar
+openhpc.tar
+xcat.tar
+l_BaseKit_p_2022.1.2.146_offline.sh
+l_HPCKit_p_2022.1.2.117_offline.sh 
+)
+
+for ifile in ${filelist[@]}
+do
+  if [ ! -e ${package_dir}/${ifile} ] ; then
+  echo "安装包 ${ifile} 不存在!!!"
+  exit 13
+fi
+done
+
+echo "安装文件已找到！"
+echo "### 安装文件位置：" >> env.text
+echo "export package_dir=${package_dir}" >> env.text
+echo "export iso_path=${iso_path}" >> env.text
+
--- Rocky-River/ini.sh
+++ Rocky-River/ini.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-### 交互脚本，输入基本信息，产生Install.sh脚本
-### Install.sh脚本分成多个函数，env.sh（这个可以不要了）里边的的变量要作为函数的参数
+################################
+#####       功能规划       #####
+################################
+### 交互脚本，输入基本信息，产生Install.sh脚本以及env.text
+### Install.sh脚本调用多个函数，而全局变量存储在env.text里边，通过source 命令生效
+### 各个功能脚本放在functions文件夹里边
 ### 脚本有出错接着执行的功能
 ### 脚本应添加检测功能
-### 添加用户脚本
-### 添加节点脚本
+### 要产生一个添加用户脚本，放在root目录
+### 同样地添加节点脚本
+##################################
 
-echo "请输入集群名字（4~10个英文字母组成的字符串）："
+### 检查脚本文件的权限与完整性
+filelist=(
+cname.sh
+#cnetwork.sh
+#osprovision.sh
+)
 
-echo "检查名字是否合规"
+for ifile in ${filelist[@]}
+do
+  if [ ! -e ./functions/${ifile} ] ; then
+  echo "${ifile} 文件不存在!!!"
+  exit 
+fi
+done
+
+chmod +x ./functions/*.sh
+
+if [ -e env.text ] ; then
+rm env.text
+fi
+
+### 寻找安装包文件的位置,判断其完整性
+./functions/findpackage.sh
+if [ $? -ne 0 ]; then
+exit
+fi
+
+### 设置集群名字
+./functions/cname.sh $cname
+
+### 设置集群内网IP
+./functions/cnetwork.sh
+
+
+
 
 
 
