a807c01992b1d6dc627752500a5a251a6cd7411b
jingzhong fang <fangjzh@foxmail.com>
2023-07-17 09:19:08 +0800

    set nfs export home async feature

--- Rocky-River/functions/setup_nfs.sh
+++ Rocky-River/functions/setup_nfs.sh
@@ -9,7 +9,7 @@ echo "$0 执行开始！" >${0##*/}.log
 perl -pi -e "s|/tftpboot|#/tftpboot|" /etc/exports
 perl -pi -e "s|/install|#/install|" /etc/exports
 ### note: fsid should be uniq, if add dir###
-echo "/home *(rw,no_subtree_check,fsid=10,no_root_squash)" >> /etc/exports
+echo "/home *(rw,async,no_subtree_check,fsid=10,no_root_squash)" >> /etc/exports
 echo "/opt/ohpc/pub *(ro,no_subtree_check,fsid=11)" >> /etc/exports
 ##echo "/opt/repo *(ro,no_subtree_check,fsid=12)" >> /etc/exports
 exportfs -a
